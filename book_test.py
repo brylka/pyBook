312bb0acea9f2a20a9b28edf15d1a1a1f08ac401
brylka <brylka@jelenia.com>
2023-12-03 12:40:37 +0100

    stworzenie pierwszego testu

---
+++ book_test.py
@@ -0,0 +1,14 @@
+import unittest
+from book import Book
+
+
+class TestBook(unittest.TestCase):
+    def setUp(self):
+        self.book = Book("Wiedźmin", "Sapkowski", 2000)
+    def test_get_info(self):
+        text_correct = "Książka: Wiedźmin Autor: Sapkowski Rok: 2000"
+        text_result = self.book.get_info()
+        self.assertEqual(text_correct, text_result)
+
+if __name__ == '__main__':
+    unittest.main()
